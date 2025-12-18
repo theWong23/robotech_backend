6bb743232b988222ccb57fd3c34b84e76b7ceb26
theWOng23 <junior2103rcd@gmail.com>
2025-12-18 14:22:33 -0500

    modificacion en securityconfig

--- src/main/java/com/robotech/robotech_backend/security/SecurityConfig.java
+++ src/main/java/com/robotech/robotech_backend/security/SecurityConfig.java
@@ -7,7 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import java.security.SecureRandom;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -34,6 +35,11 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
