2f3bfbd0cb226c6f9e22559a76cf10a5eff2f142
theWOng23 <junior2103rcd@gmail.com>
2025-12-18 14:16:11 -0500

    cambio en userrepository

--- src/main/java/com/robotech/robotech_backend/repository/UsuarioRepository.java
+++ src/main/java/com/robotech/robotech_backend/repository/UsuarioRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
-
-    Optional<Usuario> findByCorreoAndContrasenaHash(String correo, String contrasenaHash);
     Optional<Usuario> findByCorreo(String correo);
 
     boolean existsByCorreo(String correo);
